862c8cb2b3de6f52ae13e1c851970a69a26c960d
MarayFfrost <morozovmarat4@gmail.com>
2025-01-26 22:22:22 +0300

    fixed bugs and admin_panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Sensor, Modem, Counter
 
-# Register your models here.
+admin.site.register(Modem)
+admin.site.register(Counter)
+admin.site.register(Sensor)
